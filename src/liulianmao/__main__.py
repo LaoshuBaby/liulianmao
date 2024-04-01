76852dec7bf2b862c6221a6d32faee300b25670a
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2024-04-02 00:32:49 +0800

    ci: pyinstaller can't work

--- src/liulianmao/__main__.py
+++ src/liulianmao/__main__.py
@@ -2,13 +2,30 @@ import os
 import sys
 
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
+if getattr(sys, 'frozen', False):
+    # we are running in a |PyInstaller| bundle
+    bundled_dir = sys._MEIPASS
+else:
+    # we are running in a normal Python environment
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    bundled_dir = current_dir
+parent_dir = os.path.dirname(bundled_dir)
 if parent_dir not in sys.path:
     sys.path.append(parent_dir)
 
+print(__file__)
+print(bundled_dir)
+print(parent_dir)
+print(os.getcwd())
+print(os.listdir(bundled_dir))
 
-from liulianmao.client_core import main as core
+import importlib.util
+
+# 动态加载 client_core 模块
+spec = importlib.util.find_spec('.client_core', package='liulianmao')
+core = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(core)
+
 
 FEATURE = {
     "core": True,
@@ -18,4 +35,5 @@ FEATURE = {
 if FEATURE["langchain"]:
     from liulianmao.client_langchain import main as langchain
 
-core()
+core.main()
+
