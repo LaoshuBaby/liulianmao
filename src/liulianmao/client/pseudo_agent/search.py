7a4303a9fe34171c4cf4f50128eac65b40680bed
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2024-05-22 19:42:55 +0800

    format: black

--- src/liulianmao/client/pseudo_agent/search.py
+++ src/liulianmao/client/pseudo_agent/search.py
@@ -6,12 +6,13 @@ sys.path.append(os.path.join(current_dir, ".."))
 
 from module.log import logger
 
-def get_search_result(keyword, engine='baidu') -> str:
-    def get_search_url(keyword:str,engine:str):
-        if engine.lower() == 'baidu':
+
+def get_search_result(keyword, engine="baidu") -> str:
+    def get_search_url(keyword: str, engine: str):
+        if engine.lower() == "baidu":
             search_url = f"https://www.baidu.com/s?wd={keyword}"
             # search_url = f"https://www.bing.com/search?q={keyword}"
-        elif engine.lower() in ['google',"bing","duckduckgo","yandex"]:
+        elif engine.lower() in ["google", "bing", "duckduckgo", "yandex"]:
             # search_url = f"https://www.bing.com/search?q={keyword}"
             # search_url = f"https://duckduckgo.com/?q={keyword}"
             # search_url = f"https://www.google.com/search?q={keyword}"
@@ -19,17 +20,18 @@ def get_search_result(keyword, engine='baidu') -> str:
             raise ValueError(f"搜索引擎 {engine} 可以解析但尚未实现。")
         else:
             raise ValueError(f"搜索引擎 {engine} 无法解析。")
-        
+
         logger.trace(f"[get_search_result.keyword]: {keyword}")
         logger.trace(f"[get_search_result.engine]: {engine}")
         logger.trace(f"[get_search_result.search_url]: {search_url}")
-        
+
         return search_url
-    
+
     try:
         import requests
+
         response = requests.get(
-            url=get_search_url(keyword,engine),
+            url=get_search_url(keyword, engine),
             headers={
                 "User-Agent": "Mozilla/5.0 (liulianmao_get_search_result/0.0.1)",
                 "Referer": "https://github.com/LaoshuBaby/liulianmao/",
@@ -44,41 +46,40 @@ def get_search_result(keyword, engine='baidu') -> str:
         import re
 
         def get_text_from_url(url_raw_text):
-                from bs4 import BeautifulSoup
+            from bs4 import BeautifulSoup
 
-                soup = BeautifulSoup(url_raw_text, "html.parser")
-                return soup.get_text()
+            soup = BeautifulSoup(url_raw_text, "html.parser")
+            return soup.get_text()
 
         souped_text = get_text_from_url(response.text)
 
         logger.trace(f"[souped_text]:\n{souped_text}")
 
         def reduce_spaces(input_string, continue_space_thershold):
-                import re
+            import re
 
-                return re.sub(
-                    f" {continue_space_thershold+1,}",
-                    " " * continue_space_thershold,
-                    input_string,
-                )
+            return re.sub(
+                f" {continue_space_thershold+1,}",
+                " " * continue_space_thershold,
+                input_string,
+            )
 
         pruned_text = "\n".join(
-                list(
-                    filter(
-                        bool,
-                        [
-                            reduce_spaces(i, CONTINUE_SPACE_THERSHOLD)
-                            if (
-                                i != ""
-                                and bool(re.fullmatch(r"^[\s\n\t]*$", i))
-                                == False
-                            )
-                            else ""
-                            for i in souped_text.split("\n")
-                        ],
-                    )
+            list(
+                filter(
+                    bool,
+                    [
+                        reduce_spaces(i, CONTINUE_SPACE_THERSHOLD)
+                        if (
+                            i != ""
+                            and bool(re.fullmatch(r"^[\s\n\t]*$", i)) == False
+                        )
+                        else ""
+                        for i in souped_text.split("\n")
+                    ],
                 )
-            ).replace("\t", "")
+            )
+        ).replace("\t", "")
 
         logger.trace(f"[pruned_text]:\n{pruned_text}")
 
