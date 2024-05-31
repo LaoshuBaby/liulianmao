2977b49a7301fdf9c5f27f8616c7e3505c203699
laoshubaby <laoshubaby@protonmail.com>
2024-05-31 23:23:15 +0800

    feat: can read pdf

--- src/liulianmao/client/utils/file.py
+++ src/liulianmao/client/utils/file.py
@@ -21,6 +21,29 @@ def local_file_reader(path_list: List[str], flag_recursive=False) -> str:
 
     logger.trace(f"[local_file_reader().path_list]: {path_list}")
 
+    def read_single_file_pdf(file_path):
+        """读取PDF文件并返回其内容作为纯文本字符串。
+
+        Args:
+            file_path (str): PDF文件路径。
+
+        Returns:
+            str: PDF文件内容作为纯文本字符串。
+        """
+        import pymupdf  # PyMuPDF
+
+        text_content = []
+        try:
+            doc = pymupdf.open(file_path)
+            for page in doc:
+                text = page.get_text()
+                text_content.append(text)
+        except Exception as e:
+            logger.error(f"Error while extracting text from PDF: {e}")
+            return str(e)
+
+        return "\n".join(text_content)
+
     def read_single_file(file_path: str) -> Optional[str]:
         """读取单个文件内容。
 
@@ -32,7 +55,14 @@ def local_file_reader(path_list: List[str], flag_recursive=False) -> str:
         """
         try:
             with open(file_path, "r", encoding="utf-8") as file:
-                return file.read()
+                if file_path.lower().endswith(".pdf"):
+                    # 如果是PDF，调用read_single_file_pdf来处理
+                    logger.info("[file_type]: PDF")
+                    return read_single_file_pdf(file_path)
+                elif file_path.lower().endswith(".jpg"):
+                    logger.info("[file_type]: IMG")
+                else:
+                    return file.read()
         except Exception as e:
             logger.error(e)
             logger.warning(
