0d1044a0a19704d21f5c6ad5689db5df5e645b62
laoshubaby <laoshubaby@protonmail.com>
2024-06-05 07:44:17 +0800

    init: zhipu batch API
    https://open.bigmodel.cn/dev/api#batch
    
    突然发现6.9以前用batch API限时免费哎！！！

--- src/liulianmao/client/api/zhipu.py
+++ src/liulianmao/client/api/zhipu.py
@@ -99,3 +99,68 @@ def zhipu_completion(
             f"Error: {response.status_code} {response.content.decode('utf-8')}"
         )
         return {}
+
+
+def zhipu_batch(
+    prompt_question: str,
+    prompt_system: str,
+    model: str = "glm-4",
+    amount: int = 1,
+    no_history: bool = False,
+    completion_window="24h",
+):
+    if no_history:
+        append_conversation = []
+    else:
+        append_conversation = conversation
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {API_KEY}",
+    }
+    payload = {
+        "model": "glm-4",
+        "messages": [
+            {"role": "system", "content": prompt_system},
+        ]
+        + append_conversation
+        + [{"role": "user", "content": prompt_question}],
+    }
+    logger.trace("[Headers]\n" + f"{headers}")
+    logger.trace("[Payload]\n" + f"{payload}")
+    response = requests.post(
+        API_URL + "/paas/v4/chat/completions", headers=headers, json=payload
+    )
+
+    if response.status_code == 200:
+        logger.trace("[Debug] response.status_code == 200")
+        # judge content_type
+        try:
+            logger.trace("[Response]\n" + str(response.json()))
+        except Exception as e:
+            logger.trace(e)
+            logger.critical("RESPONSE NOT JSON")
+        # judge json schema
+        try:
+            if no_history == False:
+                conversation.append(
+                    {"role": "user", "content": prompt_question}
+                )
+                conversation.append(
+                    {
+                        "role": "system",
+                        "content": response.json()["choices"][0]["message"][
+                            "content"
+                        ],
+                    }
+                )
+        except Exception as e:
+            logger.trace(e)
+            logger.critical("WRONG RESPONSE SCHEMA")
+        logger.trace("[History]\n" + str(conversation))
+        return response.json()
+    else:
+        logger.trace("[Debug] response.status_code != 200")
+        logger.error(
+            f"Error: {response.status_code} {response.content.decode('utf-8')}"
+        )
+        return {}
