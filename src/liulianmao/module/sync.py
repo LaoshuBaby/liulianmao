cbd066390f30a4d2c28f91ee33e7734b98ba3f7c
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2024-06-23 10:00:30 +0800

    feat: change to OOP model for S3 and S3 capable service
    果然还是只有OOP的继承模型能更好的体现出儿子随爹的效果
    
    minio：别吵，我宣布个事——大家都是狗

--- src/liulianmao/module/sync.py
+++ src/liulianmao/module/sync.py
@@ -6,29 +6,46 @@ from .log import logger
 from .storage import PROJECT_FOLDER, get_user_folder
 
 
-default_profile_name="apikey.liulianmao"
-default_apikey=os.path.join(get_user_folder(), PROJECT_FOLDER, default_profile_name)
+default_profile_name = "apikey.liulianmao"
+default_apikey = os.path.join(
+    get_user_folder(), PROJECT_FOLDER, default_profile_name
+)
 
-def driver_aws_s3(accesskey=""):
-    pass
 
-def driver_aliyun_oss(accesskey=""):
-    """
-    S3 alternative
-    """
-    pass
+class DriverAWSS3:
+    def __init__(
+        self,
+        endpoint: str,
+        accesskey: str,
+        access_serect: str,
+        region="",
+        bucket="",
+        path="",
+    ):
+        pass
 
-def driver_tencent_cos(accesskey=""):
-    """
-    S3 alternative
-    """
-    pass
 
-def driver_github_gist():
-    pass
-    
+class DriverAliyunOSS(DriverAWSS3):
+    def __init__(self):
+        pass
 
-def sync_profiles(target_profile_list:List[str]=[default_apikey]):
+
+class DriverTencentCOS(DriverAWSS3):
+    def __init__(self):
+        pass
+
+
+class DriverGitHubGist:
+    def __init__(self):
+        pass
+
+
+class DriverGit:
+    def __init__(self):
+        pass
+
+
+def sync_profiles(target_profile_list: List[str] = [default_apikey]):
     """
     本函数主要用于进行配置文件同步
     以确保在跨设备工作时为不同模型不同endpoint配置的apikey
