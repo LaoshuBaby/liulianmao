cee1af8299268001cadb46ca5505f5f2ae61dfff
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2024-06-23 11:08:09 +0800

    feat: more and more details about sync, but indeed re-invent wheel

--- src/liulianmao/module/sync.py
+++ src/liulianmao/module/sync.py
@@ -1,6 +1,8 @@
 import os
 from typing import Dict, List
 
+import requests
+
 from .log import logger
 from .storage import PROJECT_FOLDER, get_user_folder
 
@@ -16,10 +18,11 @@ class DriverAWSS3:
         endpoint: str,
         accesskey: str,
         access_serect: str,
-        region="",
-        bucket="",
-        path="",
+        region: str,
+        bucket: str,
+        path="/",
     ):
+        logger.debug("[Sync]Driver AWS S3 was created!")
         pass
 
     def check_path_style(self) -> str:
@@ -32,6 +35,78 @@ class DriverAWSS3:
         else:
             return "path_style"
 
+    def read(self, method: str = "restful") -> str:
+        content = ""
+        return content
+
+    def write(self, content: str, method: str = "restful") -> Dict[str, str]:
+        response = {}
+        return response
+
+
+class DriverAliyunOSS(DriverAWSS3):
+    def __init__(self, method="compatiable"):
+        if method == "native":
+            # use aliyun sdk method
+            import oss2
+        elif method == "restful":
+            # use http request
+            super.read(method="restful")
+            pass
+        else:
+            # compatiable method
+            super.read(method="compatiable")
+            pass
+
+
+class DriverTencentCOS(DriverAWSS3):
+    def __init__(self):
+        pass
+
+
+class DriverGit:
+    def __init__(self):
+        logger.debug("[Sync]Driver Git repository was created!")
+        pass
+
+
+class DriverPastebin:
+    """
+    注意：这里的pastebin不是Ubuntu Pastebin之类的，因为它们是只读的不可反复对同一个地址更新
+    是为了统一GitHub、Gitee的Gist和GitLab的Snippets在称呼上的不同。
+
+    文档：
+    * GitHub: https://docs.github.com/en/rest/gists
+    * GitLab: https://docs.gitlab.com/ee/user/snippets.html
+    * Gitee: https://gitee.com/api/v5/swagger#/getV5Gists
+
+    这里如果出现GitLab，特指GitLab official，如果是自部署的请自行传参
+    """
+
+    endpoint_collection = {
+        "github": "https://gist.github.com/",
+        "gitlab": "https://snippet.gitlab.com/",
+        "gitee": "做梦呢小可爱？今天必须报这个Exception让你爱死什么叫特供平台",
+    }
+
+    def _get_endpoint(self, x: str = "GitHub") -> str:
+        available_endpoint_presets = [key for key in self.endpoint_collection]
+        if x.lower() in available_endpoint_presets:
+            return self.endpoint_collection.get(x.lower())
+        else:
+            return x
+
+    def __init__(
+        self,
+        pastebin_token: str,
+        pastebin_id: str,
+        endpoint: str = _get_endpoint("GitHub"),
+        alt_name: str = "",
+    ):
+        logger.debug("[Sync]Driver Pastebin was created!")
+
+        pass
+
     def read(self) -> str:
         content = ""
         return content
@@ -41,51 +116,30 @@ class DriverAWSS3:
         return response
 
 
-class DriverAliyunOSS(DriverAWSS3):
-    def __init__(self, method="compatiable"):
-        if method == "native":
-            pass
-            # import aliyun_sdk_python_oss
-        pass
-
-
-class DriverTencentCOS(DriverAWSS3):
-    def __init__(self):
-        pass
-
-
-class DriverGitHubGist:
-    def __init__(self):
-        pass
-
-
-class DriverGit:
-    endpoint_collection = {
-        "github": "https://gist.github.com/",
-        "gitlab": "https://gist.gitlab.com/",
-        "gitlab_official": "",
-        "gitee": "做梦呢小可爱？今天必须报这个Exception让你爱死什么叫特供平台",
-    }
-
-    def _get_endpoint(self, name: str = "GitHub") -> str:
-        return self.endpoint_collection.get(name.lower())
-
-    def __init__(
-        self,
-        gist_token: str,
-        gist_id: str,
-        endpoint: str = _get_endpoint("GitHub"),
-        alt_name: str = "",
-    ):
-        pass
-
-
-def sync_profiles(target_profile_list: List[str] = [default_apikey], driver:str="s3.aws"):
+def sync_profiles(
+    target_profile_list: List[str] = [default_apikey],
+    driver: str = "s3.aws",
+    **kwargs
+):
     """
     本函数主要用于进行配置文件同步
     以确保在跨设备工作时为不同模型不同endpoint配置的apikey
     均可快速迁移，无需反复配置。
+
+    命名为apikey是因为accesskey要在S3里面用，token在LLM领域有特指含义，访问控制的令牌只能叫apikey了
+
+    此外，这个文件就是重新实现了 https://github.com/mfuentesg/SyncSettings 果然又一次重新发明了轮子
     """
-    if driver=="s3.aws":
-        driver=DriverAWSS3(endpoint="",accesskey="114514",access_serect="1919810")
-    pass
+    if driver == "s3.aws":
+        driver = DriverAWSS3(
+            endpoint="",
+            accesskey="114514",
+            access_serect="1919810",
+            region="",
+            bucket="",
+            path="/",
+        )
+    elif driver == "pastebin":
+        driver = DriverPastebin(
+            pastebin_id="", pastebin_token="", endpoint="GitHub"
+        )
