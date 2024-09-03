00b2bec67b5b71008e7369b6951bdd24d13ddb44
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2024-09-03 22:36:03 +0800

    feat: call cogview model

--- src/liulianmao/client/api/zhipu.py
+++ src/liulianmao/client/api/zhipu.py
@@ -8,6 +8,8 @@ sys.path.append(os.path.join(current_dir, ".."))
 
 from module.authentication import API_KEY, API_URL
 from module.log import logger
+from module.storage import PROJECT_FOLDER, get_user_folder, init
+
 
 conversation = []
 
@@ -271,3 +273,157 @@ def zhipu_batch(
             f"Error: {response.status_code} {response.content.decode('utf-8')}"
         )
         return {}
+
+
+
+def openai_images_generations(
+    prompt,
+    model: str = "cogview-3-plus",
+    size: str = "1024x1024",
+    quality: str = "standard",
+    amount: int = 1,
+):
+    headers = {
+        "Authorization": f"Bearer {API_KEY}",
+        "Content-Type": "application/json",
+    }
+
+    payload = {
+        "model": model,
+        "prompt": prompt,
+        "size": size,
+        "quality": quality,
+        "n": amount,
+    }
+
+    logger.trace("[Headers]\n" + f"{headers}")
+    logger.trace("[Payload]\n" + f"{payload}")
+
+    response = requests.post(
+        API_URL + "/paas/v4/images/generations", headers=headers, json=payload
+    )
+
+    def checker_size(size:str)->bool:
+        if size in ["1024x1024","768x1344","864x1152","1344x768","1152x864","1440x720","720x1440"]:
+            return True
+        else:
+            return False
+
+
+    def download_image(url, save_path):
+        from urllib.parse import parse_qs, urlparse
+
+        parsed_url = urlparse(url)
+        file_name = parsed_url.path.split("/")[-1]
+
+        # 检查并根据内容类型更改文件后缀
+        file_params = parse_qs(parsed_url.query)
+        logger.trace(f"[extension_check:file_params]: {file_params}")
+        response = requests.head(url)
+        logger.trace(f"[extension_check:response.headers]: {response.headers}")
+
+        def get_content_type():
+            if file_params.get("rsct", []) != []:
+                return str(file_params["rsct"][0])
+            elif "Content-Type" in response.headers:
+                return str(response.headers["Content-Type"])
+            else:
+                return ""
+
+        if get_content_type():
+            content_type = get_content_type()
+            file_name_ext = content_type.split("/")[-1]
+            file_name_stem = os.path.splitext(file_name)[0]
+            file_name = f"{file_name_stem}.{file_name_ext}"
+
+            def generate_unique_file_name(
+                save_path, file_name_stem, file_name_ext
+            ):
+                full_file_name = f"{file_name_stem}.{file_name_ext}"
+                full_save_path = os.path.join(save_path, full_file_name)
+                file_number = 0
+
+                # 检查文件是否存在，如果存在则在文件名中增加编号
+                if os.path.exists(full_save_path):
+                    logger.trace(
+                        f"[file_name:collision.number]: {full_save_path}"
+                    )
+                    logger.trace(
+                        f"[file_name:collision.exist]: {full_save_path}"
+                    )
+                    while os.path.exists(full_save_path):
+                        logger.trace(
+                            f"[file_name:collision.number]: {full_save_path}"
+                        )
+                        logger.trace(
+                            f"[file_name:collision.exist]: {full_save_path}"
+                        )
+                        file_number += 1
+                        full_file_name = (
+                            f"{file_name_stem}({file_number}).{file_name_ext}"
+                        )
+                        full_save_path = os.path.join(
+                            save_path, full_file_name
+                        )
+
+                    return f"{file_name_stem}({file_number}).{file_name_ext}"
+                else:
+                    return f"{file_name_stem}.{file_name_ext}"
+
+            file_name = generate_unique_file_name(
+                save_path, file_name_stem, file_name_ext
+            )
+        else:
+            logger.warning(
+                "haven't get image content_type and use default filename"
+            )
+            file_name = file_name
+        logger.trace(f"[file_name]: {file_name}")
+
+        # 下载文件
+        response = requests.get(url)
+        if response.status_code == 200:
+            full_save_path = os.path.join(save_path, file_name)
+            with open(full_save_path, "wb") as f:
+                f.write(response.content)
+            print(f"File downloaded as {full_save_path}")
+        else:
+            print("Failed to download the file.")
+
+    if response.status_code == 200:
+        content_type = response.headers.get("Content-Type", "")
+
+        if "application/json" in content_type:
+            try:
+                url_list_json = response.json().get("data", [])
+                for url_item in url_list_json:
+                    url = url_item.get("url")
+                    logger.trace(f"[img_url]: {url}")
+
+                    if url:
+                        filename = url.split("/")[-1]
+                        save_path = os.path.join(
+                            get_user_folder(), PROJECT_FOLDER, "images"
+                        )
+                        logger.trace(f"[save_path]: {save_path}")
+                        download_image(url, save_path)
+                    else:
+                        logger.warning("找不到URL。")
+            except ValueError as e:
+                logger.error("解析JSON失败。")
+                logger.error(str(e))
+        elif "image/png" in content_type:
+            img_file_path = os.path.join(
+                get_user_folder(),
+                PROJECT_FOLDER,
+                "images",
+                "downloaded_image.png",
+            )
+            with open(img_file_path, "wb") as f:
+                f.write(response.content)
+            logger.success("图片文件保存成功。")
+        else:
+            logger.warning("未知的内容类型。")
+    else:
+        logger.error(f"响应状态码错误：{response.status_code}")
+        logger.error(response.content.decode("utf-8"))
