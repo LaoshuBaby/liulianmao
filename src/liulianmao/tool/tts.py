22e874a3e03ddb2b437cb8a8d376f18e81f0ce5c
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2025-03-25 23:49:41 +0900

    feat: allow multi speaker
    references:
    * https://www.assemblyai.com/docs
    * https://community.openai.com/t/can-whisper-distinguish-two-speakers/291253/4

--- src/liulianmao/tool/tts.py
+++ src/liulianmao/tool/tts.py
@@ -1,14 +1,19 @@
-# `pip3 install assemblyai` (macOS)
-# `pip install assemblyai` (Windows)
-
 import assemblyai as aai
 from liulianmao import logger
 
-aai.settings.api_key = input()
+aai.settings.api_key = input("Please input assemblyai apikey")
 transcriber = aai.Transcriber()
+config = aai.TranscriptionConfig(speaker_labels=True)
+transcript = transcriber.transcribe(input("Please input URI you want to transcribe"),config=config)
 
-transcript = transcriber.transcribe(input())
-# transcript = transcriber.transcribe("https://assembly.ai/news.mp4")
-# transcript = transcriber.transcribe("./my-local-audio-file.wav")
+# demo
+# transcriber.transcribe("https://assembly.ai/news.mp4")
+# transcriber.transcribe("./my-local-audio-file.wav")
 
-logger.info(transcript.text)
+
+if transcript.status == aai.TranscriptStatus.error:
+    print(transcript.error)
+else:
+    # logger.info(transcript.text) # 如果没带config
+    for utterance in transcript.utterances:
+        logger.info(f"Speaker {utterance.speaker}: {utterance.text}")
