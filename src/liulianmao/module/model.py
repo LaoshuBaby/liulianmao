db3031405d2bfb07dc484ff349d497fd49b88132
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2025-08-02 13:37:24 +0800

    fix(model.py): remove ratio and may even change everything to direct choose
    The fallback list may be too out-of-date and we can't preset all series all model, and can't update once a new model release

--- src/liulianmao/module/model.py
+++ src/liulianmao/module/model.py
@@ -3,7 +3,6 @@ from .log import logger
 # 合并后的模型和变体信息
 MODEL_INFO = {
     "gpt-3.5-turbo": {
-        "ratio": 0.75,
         "variants": [
             "gpt3",
             "gpt-3",
@@ -15,11 +14,9 @@ MODEL_INFO = {
         ],
     },
     "gpt-3.5-turbo-16k": {
-        "ratio": 1.5,
         "variants": ["gpt-3.5-turbo-16k", "gpt-3.5-turbo-16k-0613"],
     },
     "gpt-4-turbo-preview": {
-        "ratio": 5.00,
         "variants": [
             "gpt-4-1106-preview",
             "gpt-4-0125-preview",
@@ -28,15 +25,12 @@ MODEL_INFO = {
         ],
     },
     "gpt-4": {
-        "ratio": 15.00,
         "variants": ["gpt4", "GPT-4", "gpt-4-0314", "gpt-4-0613"],
     },
     "gpt-4-32k": {
-        "ratio": 30.00,
         "variants": ["gpt-4-32k", "gpt-4-32k-0314", "gpt-4-32k-0613"],
     },
     "gpt-4-vision-preview": {
-        "ratio": None,  # Assuming we don't have a ratio for this model yet
         "variants": ["gpt-4-vision-preview"],
     },
 }
@@ -45,12 +39,11 @@ MODEL_INFO = {
 def select_model(
     input_model_name, available_models, direct_debug: bool = False
 ):
-    def log_and_return(model_name, ratio="NaN"):
+    def log_and_return(model_name):
         """
         辅助函数，用于记录日志并返回模型名称。
-        如果在直接调试模式下，比率记录为NaN；否则，记录实际的比率。
         """
-        logger.info(f"[Model] {model_name} ({ratio} x)")
+        logger.info(f"[Model] {model_name}")
         return model_name
 
     if direct_debug:
@@ -67,7 +60,7 @@ def select_model(
                 # 选择最新的变体
                 selected_variant = max(available_variants)
                 return log_and_return(
-                    selected_variant, model_info.get("ratio")
+                    selected_variant
                 )
 
     logger.error("[Model] Model not found.")
